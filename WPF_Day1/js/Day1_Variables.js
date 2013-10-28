69358dffdefd9c9aeb9ffa0a922b03781fcfddb6
mstiernelof <mstiernelof@Maries-MacBook-Pro.local>
2013-10-28 11:36:16 -0400

    commentsandvariables
    Class 1: 2 types of comments, variables intro.

---
+++ WPF_Day1/js/Day1_Variables.js
@@ -0,0 +1,13 @@
+/**
+ * Created by mstiernelof on 10/28/13.
+ */
+// Comment type 1 one line
+
+/* Comment type 2 allows multiple lines */
+
+
+var name;
+name ="Marie Stiernelof";
+
+console.log(name);
+
