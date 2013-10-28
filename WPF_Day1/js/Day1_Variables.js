f54d5b0d930a8f8b26fae6e4dbfd3e9843374234
mstiernelof <mstiernelof@Maries-MacBook-Pro.local>
2013-10-28 11:52:11 -0400

    numerictypecasting
    Class 1 covered numbers

--- WPF_Day1/js/Day1_Variables.js
+++ WPF_Day1/js/Day1_Variables.js
@@ -6,8 +6,23 @@
 /* Comment type 2 allows multiple lines */
 
 
+
+
+
+
 var name;
-name ="Marie Stiernelof";
+name ="Marie Stiernelof"; //string example
+var age = 24; //number example
+var pet = "Thor";
+var petAge = 2;
 
-console.log(name);
 
+console.log(age);
+age = age +1;
+console.log(age);
+age = age -10;
+console.log(age);
+age = age*2;
+console.log(age);
+
+console.log(parseInt(petAge) + Number(age));
