3c2ad83f217e1b62e64d1b02bb62f06ee58bdadb
Marie Stiernelof <mstiernelof@gmail.com>
2013-11-04 10:27:41 -0500

    Day4_conditional
    Practicing if else statements

--- WPF_Day1/WPF_Day3/Day3_loops.js
+++ WPF_Day1/WPF_Day3/Day3_loops.js
@@ -10,12 +10,26 @@
 //}
 
 
+var arr = [];
+arr [0]=[];
+arr [1]=[];
+arr [2]=[];
+arr [3]=[];
+
+arr [0]=[];
+arr [1]=[];
+arr [2]=[];
+arr [3]=[];
+
 var number=parseInt(prompt("Give me a number"));
 
 
 
-for(i=1;i<=number;i=i+1){
-    console.log(i+5);
+for(i=1;i<=3;i++){
+   for(j=0;j<3;j++){
+    console.log(i+':'+j);
+    arr[i][j]=arr[0][i]*arr[j][0];
+   }
 }
 
 
---
+++ WPF_Day1/WPF_Day3/Day4_conditional.js
@@ -0,0 +1,29 @@
+/**
+ * Created by mstiernelof on 11/4/13.
+ */
+//
+//for(i=0;i<10;i++){
+//    if(i%2=0){
+//        console.log(i)
+//    }
+//}
+
+//var qu = prompt("Grey or Blue?");
+//
+//
+//
+//if (qu.toUpperCase()=='GREY'){
+//    console.log('Grey');
+//} else {console.log('Blue');}
+
+
+//math.random practice
+
+var number = (Math.random);
+
+if (number>=.5){
+    console.log("Low");
+} else {console.log("High");}
+
+
+
