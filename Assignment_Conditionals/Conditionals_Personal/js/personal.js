4f89de3ca2826cc2927fcb7ac777468158042490
Marie Stiernelof <mstiernelof@gmail.com>
2013-11-15 00:04:04 -0500

    Conditionals_Personal
    Moved result console log into conditional statement. Has not solved the
    problem. Validiation/Conditional must still be fixed.

--- Assignment_Conditionals/Conditionals_Personal/js/personal.js
+++ Assignment_Conditionals/Conditionals_Personal/js/personal.js
@@ -2,7 +2,7 @@
  *  Created by marie stiernelof 11/13/13 Assignment: Conditionals
  */
 //create code that calculates a result
-//submits 3
+//submits 4
 //what will code calculate
 //2 givens that should be prompted
 //At least two factors must be essential for the calculation.
@@ -41,10 +41,10 @@ if (hoursPerWeek == ""){
     console.log("Not enough adequate information provided. Please enter a number for hours per week")
 }else if (wagePerWeek == ""){
     console.log("Not enough adequate information provided. Please enter a number for pay per hour")
-}
+}else {console.log("I will have to work "+weeksUntilGoal+" weeks to afford surgery for my cat.");}
 //starting work on the conditional part of the assignment
 
-console.log("I will have to work "+weeksUntilGoal+" weeks to afford surgery for my cat.")
+
 //Logging the result to the console and making sure that the result is explained
 // and that there is proper spacing for better readability.
 
