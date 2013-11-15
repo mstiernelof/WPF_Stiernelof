83853eeee3c1beea0359c312e76b48a91a0c1ce1
Marie Stiernelof <mstiernelof@gmail.com>
2013-11-14 23:00:02 -0500

    Assignment_Conditionals
    Notes to self to remind me of assignment specifications. Started
    Conditionals_Personal. Decided what code will calculate.

--- Assignment_Conditionals/Conditionals_Personal/js/personal.js
+++ Assignment_Conditionals/Conditionals_Personal/js/personal.js
@@ -1,3 +1,33 @@
 /**
  *  Created by marie stiernelof 11/13/13 Assignment: Conditionals
  */
+//create code that calculates a result
+
+//what will code calculate
+//2 givens that should be prompted
+//At least two factors must be essential for the calculation.
+// If one of the factors (the pieces of information you are collecting with the prompts)
+// is not needed in order to calculate the result then it is not essential.
+//declare variable for the result
+//Calculate the result using the variables in an expression (NOT the literal values or numbers)
+//Result should appear in the browser's console. With an explanation, include units of measurement
+
+//!! Must use conditionals to validate the information
+//Ex. if the user does not enter a value for the first prompt, make a conditional that checks if
+// the value is empty. You can do this by evaluating if the contents of the prompt are the same
+// as an empty string (which looks like "" i.e. thats two quotations with no character between them)
+//comment on every line explaining what is being done
+
+//Each of the following must be used in in at least one of the 3 projects
+//1.ternary 2.else if 3.logical operator 4.relational operator other than ==
+//20 commits for each project!
+
+//If I work X hours a week and get paid X dollars an hour. How long will I have to work to
+// be able to afford $1000 surgery for my cat.
+
+var hoursPerWeek = parseInt(prompt("How many hours per week do you work?"))
+var wagePerHour = parseInt(prompt("How much do you get paid per hour?"))
+var surgeryCost = 1000
+
+var wagePerWeek = hoursPerWeek * wagePerHour
+var weeksUntilGoal = 1000/wagePerWeek
