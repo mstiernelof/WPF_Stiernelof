d8308bbdf136276e15fb95f87e71ff9bc9e12dc0
Marie Stiernelof <mstiernelof@gmail.com>
2013-11-16 23:32:23 -0500

    Expressions_Industry
    Decided on Fashion Merchandising industry expression

--- Assignment_expressions/Expressions_Industry/js/industry.js
+++ Assignment_expressions/Expressions_Industry/js/industry.js
@@ -1,9 +1,18 @@
 /**
  * Created by marie stiernelof on 11/15/13. Assignment:Expressions - Industry
  */
-//commit 0
+//commit 1
 //math expression, 3 factors as variables (prompted) 3 arithmetic operators
 //prompted value must parseInt
 //console output include calculated values, supporting text, string concatenation
 //one of each variable with meaningful names (1.array 2.string, 3.number)
 //20 submits with meaningful comments
+
+//Fashion Merchandising, Calculate the markup amount of a pair of designer jeans
+
+//markup% = markup amount / retail price
+// 300%=markup amount/$168
+//var retailPrice
+//var cost
+//var markupDollar
+
