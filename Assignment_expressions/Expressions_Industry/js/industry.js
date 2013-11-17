340b7e6ae972bf265cd9dd60376da2912fb1b802
Marie Stiernelof <mstiernelof@gmail.com>
2013-11-16 23:52:29 -0500

    Expressions_Industry
    wrote expression to calculate mark up amount

--- Assignment_expressions/Expressions_Industry/js/industry.js
+++ Assignment_expressions/Expressions_Industry/js/industry.js
@@ -1,7 +1,7 @@
 /**
  * Created by marie stiernelof on 11/15/13. Assignment:Expressions - Industry
  */
-//commit 5
+//commit 6
 //math expression, 3 factors as variables (prompted) 3 arithmetic operators
 //prompted value must parseInt
 //console output include calculated values, supporting text, string concatenation
@@ -12,9 +12,6 @@
 
 //markup% = markup amount / retail price
 // 300%=markup amount/$168
-//var retailPrice
-//var cost
-//var markupDollar
 
 var retailPrice = parseInt(prompt("What is the retail price of those designer jeans?"));
 //Prompted variable for the retail price of the jeans. parseInt to make sure its a number
@@ -24,3 +21,4 @@ var markupPercent = parseInt(prompt("What is the markup percent?"));
 var markupDecimal = markupPercent / 100
 //Convert mark-up % to a number that's easier to work with in an expression
 
+var markupAmount = markupDecimal * retailPrice
