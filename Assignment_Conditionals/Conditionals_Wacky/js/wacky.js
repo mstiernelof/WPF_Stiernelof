525c906bfac02e59b335b6f9b4b8e37650b63bf0
Marie Stiernelof <mstiernelof@gmail.com>
2013-11-19 23:03:40 -0500

    Conditionals_Wacky
    first two prompted variables

--- Assignment_Conditionals/Conditionals_Wacky/js/wacky.js
+++ Assignment_Conditionals/Conditionals_Wacky/js/wacky.js
@@ -1,3 +1,31 @@
 /**
  *  Created by marie stiernelof 11/13/13 Assignment: Conditionals
  */
+//create code that calculates a result
+//what will code calculate
+//2 givens that should be prompted
+//At least two factors must be essential for the calculation.
+// If one of the factors (the pieces of information you are collecting with the prompts)
+// is not needed in order to calculate the result then it is not essential.
+//declare variable for the result
+//Calculate the result using the variables in an expression (NOT the literal values or numbers)
+//Result should appear in the browser's console. With an explanation, include units of measurement
+
+//!! Must use conditionals to validate the information
+//Ex. if the user does not enter a value for the first prompt, make a conditional that checks if
+// the value is empty. You can do this by evaluating if the contents of the prompt are the same
+// as an empty string (which looks like "" i.e. thats two quotations with no character between them)
+//comment on every line explaining what is being done
+
+//Each of the following must be used in in at least one of the 3 projects
+//1.ternary 2.else if 3.logical operator 4.relational operator other than ==
+//20 commits for each project!
+
+//1 commits
+//Spanish Greetings
+
+var time =prompt("What time of day is it?");
+
+var context = prompt("Casual or formal"?)
+
+
