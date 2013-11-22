c16fe029357132f27cbf0a0b973e1de9195b108b
Marie Stiernelof <mstiernelof@gmail.com>
2013-11-21 21:08:14 -0500

    Functions_Industry
    Prep-work. Assignment reminders.

--- Assignment_Functions/Functions_Industry/js/industry.js
+++ Assignment_Functions/Functions_Industry/js/industry.js
@@ -1,3 +1,17 @@
 /**
- * Created by mstiernelof on 11/21/13.
+ * Created by marie stiernelof on 11/21/13. Assignment Functions: Industry
  */
+
+//    create code that calculates a result.
+//    Decide what your code will calculate.
+//    Info should be collected from the user with prompts
+//    Calculate the result using the variables in an expression (NOT the literal values or numbers)
+//    result should appear in the browser's console. (Remember to include units) This should explain the result as well.
+//    Use a function as part of your code in each sub-project. This function should have at least two parameters and return a value.
+
+// Each of the following requirements must exist in at least one of the 3 subprojects:
+//    1. A ternary, 2. An else if, 3. An anonymous function
+//    4. A normal "named" function, 5. An expression with two arithmetic operators
+//    6. A function with three parameters, 7. At least one logical operator
+
+//commits 1
