413942da54fd0f361dc033ee02807f50e6be3969
Marie Stiernelof <mstiernelof@gmail.com>
2013-11-21 23:04:35 -0500

    Functions_Personal
    wrote function. validation conditional needs to be fixed

--- Assignment_Functions/Functions_Personal/js/personal.js
+++ Assignment_Functions/Functions_Personal/js/personal.js
@@ -13,6 +13,20 @@
 //    4. A normal "named" function, 5. An expression with two arithmetic operators
 //    6. A function with three parameters, 7. At least one logical operator
 
-//commits 2
+//commits 3
 
-//How many pies to make. 4 slices per person. 12 slice in a pie. How many people coming for Thanksgiving dinner.
+//Today's weather/Should I wear outerwear?
+
+var temp = prompt("What's the temperature outside?");
+
+var todaysWeather = function (temp){
+    if (temp<=50){console.log("Yes, wear a winter coat. It's freaking cold!");}
+    if (temp<=65 && temp>=51){console.log("Yes, wear a coat. It's cold");}
+    if (temp>=66 && temp<=69){console.log("Yes, wear a lighter coat it's chilly.");}
+    if (temp>=70 && temp<=75){console.log("Wear a cardigan there may be a slight chill outside.");}
+    if (temp>=76 && temp<=84){console.log("The weather is nice, don't worry about bringing a sweater.");}
+    if (temp>=85){console.log("It's freaking hot. Why would you wear a coat?!");}
+    else if (temp==""){console.log("You entered nothing. Try again.");}
+}
+
+todaysWeather(temp);
